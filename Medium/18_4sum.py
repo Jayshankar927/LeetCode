d61a76f064149577d6c578ddc9707637f678f917
Jayashankara M <jayashankara927@gmail.com>
2026-01-28 17:06:29 +0530

    updated code for 4sum problem

--- Medium/18_4sum.py
+++ Medium/18_4sum.py
@@ -17,27 +17,42 @@ Input: nums = [2,2,2,2,2], target = 8
 Output: [[2,2,2,2]]'''
 
 def foursum(nums, target):
-    def findNsum(nums, target, N, result, results):
-        if len(nums) < N or N < 2 or target < nums[0]*N or target > nums[-1]*N:  # early termination
+    def findNsum(nums, start, target, N, path, res):
+        # Early termination
+        if N < 2 or len(nums) - start < N:
             return
-        if N == 2: # two pointers solve sorted 2-sum problem
-            l,r = 0,len(nums)-1
+        if target < nums[start] * N or target > nums[-1] * N:
+            return
+
+        # Base case: 2-sum
+        if N == 2:
+            l, r = start, len(nums) - 1
             while l < r:
                 s = nums[l] + nums[r]
-                if s == target:                        
-                    results.append(result + [nums[l], nums[r]])
+                if s == target:
+                    res.append(path + [nums[l], nums[r]])
                     l += 1
-                    while l < r and nums[l] == nums[l-1]:
+                    r -= 1
+                    while l < r and nums[l] == nums[l - 1]:
                         l += 1
+                    while l < r and nums[r] == nums[r + 1]:
+                        r -= 1
                 elif s < target:
                     l += 1
-                else:                        
+                else:
                     r -= 1
-        else: # recursively reduce N
-            for i in range(len(nums)-N+1):
-                if i == 0 or (i > 0 and nums[i-1] != nums[i]):
-                    findNsum(nums[i+1:], target-nums[i], N-1, result+[nums[i]], results)
+            return
 
-    results = []
-    findNsum(sorted(nums), target, 4, [], results)
-    return results
+        # Recursive case
+        for i in range(start, len(nums) - N + 1):
+            if i > start and nums[i] == nums[i - 1]:
+                continue
+            findNsum(nums, i + 1, target - nums[i], N - 1, path + [nums[i]], res)
+
+
+    
+    nums.sort()
+
+    res = []
+    findNsum(nums, 0, target, 4, [], res)
+    print(res)
